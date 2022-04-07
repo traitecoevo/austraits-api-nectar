d832a5b2a21cde9060f9878715431a37831b79ed
Christopher Franks <c.franks@unsw.edu.au>
2022-04-07 13:11:31 +1000

    build script: austraits-api now intalling austraits master, not 'api' branch

--- userdata.sh
+++ userdata.sh
@@ -7,7 +7,7 @@ cd $workdir
 curl --silent -L https://github.com/traitecoevo/austraits-api/archive/$branch.tar.gz | tar zxf -
 
 cd austraits-api-$branch
-sed -i 's/"traitecoevo\/austraits@api"/&, dependencies=FALSE, build_vignettes=FALSE/' "API.build/API examples v1.R"
+sed -i 's/"traitecoevo\/austraits"/&, dependencies=FALSE, build_vignettes=FALSE/' "API.build/API examples v1.R"
 Rscript api_wrapper.R &
 
 while [ ! `curl --silent -I http://localhost:80/health-check | grep --count "200 OK"` -eq 1 ]; do
