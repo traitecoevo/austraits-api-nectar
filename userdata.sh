0dd38f9a56c79a0459386110e2808f4f075c97b2
Christopher Franks <c.franks@unsw.edu.au>
2022-04-21 12:14:23 +1000

    merge nginx branch

--- userdata.sh
+++ userdata.sh
@@ -4,10 +4,14 @@ workdir=/opt
 
 cd $workdir
 
+cp /tmp/nginx.conf /etc/nginx/nginx.conf
+systemctl restart nginx
+
 curl --silent -L https://github.com/traitecoevo/austraits-api/archive/$branch.tar.gz | tar zxf -
 
 cd austraits-api-$branch
 sed -i 's/"traitecoevo\/austraits"/&, dependencies=FALSE, build_vignettes=FALSE/' "API.build/API examples v1.R"
+sed -i 's/port=80/port=8000/' api_wrapper.R
 Rscript api_wrapper.R &
 
 while [ ! `curl --silent -I http://localhost:80/health-check | grep --count "200 OK"` -eq 1 ]; do
@@ -15,5 +19,5 @@ while [ ! `curl --silent -I http://localhost:80/health-check | grep --count "200
     sleep 10
 done
 
-$wc_notify --data-binary '{"status": "SUCCESS"}'
+$wc_notify --silent --data-binary '{"status": "SUCCESS"}'
 echo "*** build done"
