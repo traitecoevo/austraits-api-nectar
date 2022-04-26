9a4664b7b16eec06c6ba208b1b3f00252a6dc395
Christopher Franks <c.franks@unsw.edu.au>
2022-04-26 16:19:55 +1000

    get Austraits data from Nectar object storage before launching API (fixes #5)

--- userdata.sh
+++ userdata.sh
@@ -2,16 +2,21 @@
 
 workdir=/opt
 
-cd $workdir
-
 cp /tmp/nginx.conf /etc/nginx/nginx.conf
 systemctl restart nginx
 
-curl --silent -L https://github.com/traitecoevo/austraits-api/archive/$api_branch.tar.gz | tar zxf -
+cd /tmp && ./get-austraits-data.sh
 
+cd $workdir
+
+curl --silent -L https://github.com/traitecoevo/austraits-api/archive/$api_branch.tar.gz | tar zxf -
 cd austraits-api-$api_branch
+
 sed -i 's/"traitecoevo\/austraits"/&, dependencies=FALSE, build_vignettes=FALSE/' "API.build/API examples v1.R"
 sed -i 's/port=80/port=8000/' api_wrapper.R
+
+mkdir -p "API.build/data/austraits" && cp /tmp/austraits-*.rds "API.build/data/austraits"
+
 Rscript api_wrapper.R &
 
 while [ ! `curl --silent -I http://localhost:80/health-check | grep --count "200 OK"` -eq 1 ]; do
